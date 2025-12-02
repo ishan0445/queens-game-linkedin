ff57d7a09d3902dbee8046c85169bdc02eb031ca
Ishan Rayeen <ishan0445@gmail.com>
2025-12-02 11:03:11 +0200

    Create level581.ts

---
+++ src/utils/levels/level581.ts
@@ -0,0 +1,34 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+} from "../colors";
+
+const level1 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["B", "B", "C", "A", "D", "D", "D"],
+    ["B", "E", "C", "A", "D", "D", "D"],
+    ["B", "E", "E", "A", "D", "D", "D"],
+    ["B", "E", "E", "A", "D", "D", "D"],
+    ["B", "E", "E", "A", "D", "F", "G"],
+    ["E", "E", "E", "A", "F", "F", "G"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+  },
+  isNew: true,
+};
+
+export default level1;
